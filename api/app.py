8419265b40684036ca80a01c634971e5cee02826
caf-hedgehog <hcejynny@gmail.com>
2023-02-23 00:28:01 +0900

    delete create test data about book data

--- api/app.py
+++ api/app.py
@@ -24,9 +24,6 @@ ES_PASS = ''
 es = AsyncElasticsearch(ES_URL)
 indices_client = IndicesClient(es)
 
-#スレイピング先URLの指定
-# BOOK_URL = "https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qs=true&ptk=01&q=湊かなえ" #今回はサンプルデータとして湊かなえの本を複数検索
-
 class SearchBody(BaseModel):
     keywords: dict
     sort: list = [{"_score": "desc"}]
@@ -40,57 +37,6 @@ async def index():
 
 ########## Data ##################################################
 
-#curl -X GET "http://localhost:8002/dummy_data"
-# @app.get("/dummy_data")
-# def index():
-#     res = requests.get(BOOK_URL)
-    
-#     #文字化け防止
-#     res.encoding = res.apparent_encoding
-    
-#     html_data = BeautifulSoup(res.text, "html.parser")
-#     item_list = html_data.select('[class="list_area_wrap"] [class="heightLine-2"] a')
-    
-#     id = 0
-#     books = []
-
-#     for i in item_list:
-#         #本の情報を格納するdictを用意
-#         book_data = {
-#             "id": id,
-#             "title": "",
-#             "writer": "",
-#             "description": ""
-#         }
-
-#         #本のタイトルを格納
-#         book_data["title"] = i.text
-
-#         url = i.get('href')
-#         res = requests.get(url)
-#         #文字化け防止
-#         res.encoding = res.apparent_encoding
-#         html_data = BeautifulSoup(res.text, "html.parser")
-        
-#         #著者情報を格納
-#         book_data["writer"] = (html_data.select('[class="infobox ml10 mt10"] > ul > li > a'))[0].text
-
-#         #本の詳細（あらすじ）を格納 :予約商品や電子版などたまにデータが存在しない場合あり:
-#         try:
-#             book_data["description"] = (html_data.select('[itemprop="description"]'))[0].text
-#         except:
-#             book_data["description"] = "None"
-        
-#         books.append(book_data)
-#         id += 1
-
-#     pd.set_option('display.unicode.east_asian_width', True)
-#     df = pd.io.json.json_normalize(books)
-#     df.to_csv('data/result.csv', index=False)
-
-#     return JSONResponse(
-#         content={"msg": "input data OK"}, status_code=status.HTTP_200_OK
-#     )
 
 def format_detail(text):
     fake = Faker('jp-JP')
